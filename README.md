6fb67c606fac46a40474802292ea2e19e0536808
Kordian <kordian.kucharek@student.wat.edu.pl>
2020-05-04 11:36:45 +0200

    Wykonano zadanie 2.2.1 – pojedyncza dioda LED D13

--- README.md
+++ README.md
@@ -13,4 +13,5 @@ Numer albumu: 69102
 Grupa studencka: WELE18Q5S1
 
 # Pytania do zadań z instrukcji
-...
+Zadanie 2-2-1
+W celu określenia czasu trwania opóźnienia zrealizowanego z użyciem pętli należy zmienić wartość zmiennej 'i'.
---
+++ Zadanie-2-2/src/main.cpp
@@ -0,0 +1,21 @@
+#include <avr/io.h>
+
+int main(){
+  uint32_t i;
+  DDRB |= (1<<5); //pinMode(13, OUTPUT);
+  while(1)
+  {
+    PORTB |= (1<<5); //digitalWrite(13,HIGH);
+    i= 0x3FFFF;
+    do
+    {
+      __asm__ __volatile__("nop");
+    } while(i--);
+    PORTB &= !(1<<5); // digitalWrite(13,LOW);
+    i=0x3FFFF;
+    do
+    {
+      __asm__ __volatile__("nop");  
+    } while(i--);
+  }
+}
