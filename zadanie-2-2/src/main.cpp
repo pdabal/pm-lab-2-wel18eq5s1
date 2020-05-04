b0222fb4f0fec1728262674a6abc3c3c1771488c
Korneliusz Dudus <korneliusz.dudus@student.wat.edu.pl>
2020-05-04 12:32:25 +0200

    wykonano zadanie 2.2.3 - wskaźnik

--- zadanie-2-2/src/main.cpp
+++ zadanie-2-2/src/main.cpp
@@ -1,27 +1,42 @@
 #include <avr/io.h>
-#define LED_LENGTH 8
+
+uint8_t ledState[] = {0xFF, 0x7E, 0x3C, 0x18, 0x00, 0X18, 0x3C, 0x7E};
+uint8_t *pLedState = ledState;
+bool state = true;
+
+void togglePinD13(bool *state)
+{
+  PORTB = (*state << 5);
+  *state = !(*state);
+}
+
+void delay()
+{
+  for (uint32_t j = 0x2FFFF;j >0;j--)
+  {
+    __asm__ __volatile__("nop");
+  }
+}
 
 int main()
 {
-  DDRD |= 0xFF; // Ustawiamy wszystkie wyprowadzenia portu D jako wyjścia
-  while (1)
-  { // kierunek D0 -> D7
-    for (uint8_t i =0; i< LED_LENGTH; i++)
+  DDRB |= (1 << 5);
+  DDRD |= 0xFF;
+  while(1)
+  {
+    togglePinD13(&state);
+    delay();
+    for(uint8_t i =0;i < sizeof(ledState);i++)
     {
-      PORTD = (1 << i);
-      for (uint32_t j = 0x1FFFF; j>0;j--)
-      {
-        __asm__ __volatile__("nop");
-      }
+      PORTD = ledState[i];
+      delay();
     }
-    // kireunek D6 -> D1
-    for (uint8_t i =1;i < (LED_LENGTH-1);i++)
+    pLedState = ledState;
+    for (uint8_t i = 0; i < sizeof(ledState);i++)
     {
-      PORTD = (PORTD >> 1);
-      for (uint32_t j = 0x1FFFF;j>0;j--)
-      {
-        __asm__ __volatile__("nop");
-      }
+      PORTD = *pLedState;
+      pLedState++;
+      delay();
     }
   }
 }
