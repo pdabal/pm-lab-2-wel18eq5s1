f81058100a9c17c9ead3371d4ea662535730bc55
Kordian <kordian.kucharek@student.wat.edu.pl>
2020-05-04 12:07:36 +0200

    Wykonano zadanie 2.2.3 – wskaźniki

--- Zadanie-2-2/src/main.cpp
+++ Zadanie-2-2/src/main.cpp
@@ -1,26 +1,41 @@
 #include <avr/io.h>
-#define LED_LENGHT 8
 
-int main(){
-  DDRD |= 0xFF;
-  while(1)
-  { //kierunek D0 -> D7
-    for (uint8_t i = 0;i < LED_LENGHT; i++)
-    {
-      PORTD = (1<<i);
-      for(uint32_t j=0x3FFFF; j >0;j--)
-      {
-        __asm__ __volatile("nop");
-      }
-    }
-    //kierunek D6 -> D1
-    for (uint8_t i = 1;i < (LED_LENGHT - 1); i++)
-    {
-      PORTD = (PORTD >>1);
-      for(uint32_t j=0x3FFFF; j >0;j--)
-      {
-        __asm__ __volatile("nop");
-      }
-    }
+uint8_t ledState[]= {0xFF,0x7E, 0x3C, 0x18, 0x00, 0x18, 0x3C, 0x7E};
+uint8_t *pLedState = ledState;
+bool state = true;
+
+void togglePinD13(bool *state)
+{
+  PORTB |= (*state<<5);
+  *state = !(*state);
+}
+
+void delay()
+{
+  for (uint32_t j = 0x2FFFF; j > 0; j--)
+  {
+    __asm__ __volatile("nop");
   }
 }
+
+int main()
+{
+  DDRB |= (1<<5);
+  DDRD |= 0xFF;
+  while (1)
+  {
+    togglePinD13(&state);
+    delay();
+    for (uint8_t i = 0; i < sizeof(ledState); i++)
+    {
+      PORTD = ledState[i];
+      delay();
+    }
+    for (uint8_t i = 0; i < sizeof(ledState); i++)
+    {
+      PORTD = *pLedState;
+      pLedState++;
+      delay();
+    }
+  }
+} 
