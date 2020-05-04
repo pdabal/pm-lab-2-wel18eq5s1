ba9b6129cf9720ff3dc47b64b971d5f4e655269d
Mikołaj Żochowski <mikolaj.zochowski@student.wat.edu.pl>
2020-05-04 12:39:42 +0200

    2.2.4 poprawka

--- Zadanie 2-2/src/main.cpp
+++ Zadanie 2-2/src/main.cpp
@@ -1,11 +1,12 @@
 #include <avr/io.h>
-#define SOLUTION 0
+
 uint8_t button;
 bool state = true;
 
 void togglePinD13(bool *state)
 {
   PORTB = (*state << 5);
+  *state = !(*state);
 }
 
 void delay()
@@ -17,10 +18,9 @@ void delay()
 int main()
 {
   DDRB &= !(1 << 0);
-  DDRB = (1 << 5);
+  DDRB |= (1 << 5);
   while (1)
   {
-#if (SOLUTION == 0)
   button = (PINB & (1 << PINB0));
   if (button == 0)
   {
@@ -29,13 +29,5 @@ int main()
   }
   else
     delay();
-#elif (SOLUTION == 1)
-//rozwiązanie alternatywne  
-while (!(PINB & (1 << PINB0)))
-  {
-  togglePinD13(&state);
-  delay();
-  }
-#endif
   }
 }
