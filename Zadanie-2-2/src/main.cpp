3741876a1a3fdf239bd3839733b60487d92e7e2c
Pawel Slapczynski <pawel.slapczynski@student.wat.edu.pl>
2020-05-04 10:35:36 +0200

    Utworzono projekt dla zadanie 2.2-zadanie 2-1-3

---
+++ Zadanie-2-2/src/main.cpp
@@ -0,0 +1,23 @@
+
+#include <avr/io.h>
+
+int main()
+{
+  uint32_t i;
+  DDRC |= (1<<7);
+  while (1)
+  {
+    PORTC |= (1<<7);
+    i = 0x3FFFF;
+    do
+    {
+      __asm__ __volatile("nop");
+    } while (i--);
+    PORTC &= !(1<<7);
+    i = 0x3FFFF;
+    do
+    {
+      __asm__ __volatile("nop");
+    } while (i--);
+  }
+}
