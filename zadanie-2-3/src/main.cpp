d4532e12db00e6e0cdc8c288b7ec516c0f19404e
Korneliusz Dudus <korneliusz.dudus@student.wat.edu.pl>
2020-05-04 13:29:49 +0200

    Wykonano zadanie 2.3.1 - Arduino IRQ

--- zadanie-2-3/src/main.cpp
+++ zadanie-2-3/src/main.cpp
@@ -1,9 +1,25 @@
 #include <Arduino.h>
+volatile int state = LOW;
+void myISR();
+
 int main()
 {
   init();
-  while(1)
+  pinMode(12, OUTPUT);
+  pinMode(13, OUTPUT);
+  attachInterrupt(0, myISR, CHANGE);
+  sei();
+  while (1)
   {
-    
+    digitalWrite(13, HIGH);
+    delay(500);
+    digitalWrite(13, LOW);
+    delay(500);
   }
+}
+
+void myISR()
+{
+  state = !state;
+  digitalWrite(12, state);
 }
