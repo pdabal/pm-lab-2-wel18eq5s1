213f0641ef799f7f064f4a1b05c068c4461d5443
Pawel Slapczynski <pawel.slapczynski@student.wat.edu.pl>
2020-05-04 11:19:28 +0200

    Wykonano zadanie 2.2.2-linijka świetlna led D0..D7

--- Zadanie-2-2/src/main.cpp
+++ Zadanie-2-2/src/main.cpp
@@ -1,23 +1,29 @@
 #include <avr/io.h>
+#define LED_LENGHT 8
 
 int main()
 {
-  uint32_t i;
-
-  DDRC |= (1<<7);
+  DDRD |= 0xFF;
   while (1)
   {
-    PORTC |= (1<<7);
-    i = 0x3FFFF;
-    do
+    for (uint8_t i = 0; i < LED_LENGHT; i++)
     {
+    
+      PORTD = (1<< i);
+      for (uint32_t j = 0x1FFFF; j > 0 ; j--)
+      {
       __asm__ __volatile("nop");
-    } while (i--);
-    PORTC &= !(1<<7);
-    i = 0x3FFFF;
-    do
+      }
+
+    }
+
+    for (uint8_t i = 1; i < LED_LENGHT-1; i++)
     {
+       PORTD = (PORTD >> 1);
+     for (uint32_t j = 0x1FFFF; j > 0 ; j--)
+      {
       __asm__ __volatile("nop");
-    } while (i--);
+      }
+    }
   }
 }
